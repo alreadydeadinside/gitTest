bd6aa5f526f1b1ff8a7ba46ad59dd5440d6ff88a
BoleanSwift <brytong@ukr.net>
2020-02-25 17:44:24 +0300

    created function for matrix genertation

--- anton_laba20.py
+++ anton_laba20.py
@@ -1,3 +1,11 @@
 from random import randrange
 i = int(input(" "))
 matrix=[randrange(-10, 10) for k in (i)], for k in (i)]
+
+def printMatrix(matrix):
+    for i in matrix:
+        for j in i:
+            print("{:4d}".format(j), end = " ")
+        print()
+printMatrix(matrix)
+        
