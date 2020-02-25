2c1c79220de681caa2fa77b02a6f6a1c8ff1ecdb
BoleanSwift <brytong@ukr.net>
2020-02-25 17:47:30 +0300

    randomizing stuff

--- anton_laba20.py
+++ anton_laba20.py
@@ -1,6 +1,6 @@
 from random import randrange
 i = int(input(" "))
-matrix=[randrange(-10, 10) for k in (i)], for k in (i)]
+matrix=[[randrange(-10, 10)for k in (i)]for k in (i)]
 
 def printMatrix(matrix):
     for i in matrix:
@@ -8,4 +8,9 @@ def printMatrix(matrix):
             print("{:4d}".format(j), end = " ")
         print()
 printMatrix(matrix)
-        
+
+def randomize(i):
+    for i in range(-10, 10)
+        print(i, i+1)
+
+randomize(i)
